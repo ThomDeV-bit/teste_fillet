9d788d3c21ae467544ae0cfdd39bab97014eab44
Thomaz  josetti <120723852+ThomDeV-bit@users.noreply.github.com>
2025-10-12 11:51:15 -0400

    fix: remove forbidNonWhitelisted

--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,28 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transformOptions: { enableImplicitConversion: true },
-    }),
-  );
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+            transformOptions: { enableImplicitConversion: true },
+        }),
+    );
     const config = new DocumentBuilder()
-    .setTitle('Cadastro de Clientes')
-    .setDescription('Api de Cadastro de Clientes')
-    .setVersion('1.0')
-    .addTag('clientes')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+        .setTitle('Cadastro de Clientes')
+        .setDescription('Api de Cadastro de Clientes')
+        .setVersion('1.0')
+        .addTag('clientes')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT ?? 3000);
 
-  const url = await app.getUrl()
+    const url = await app.getUrl();
 
-  console.log(`Swagger application is running on: ${url}/api`)
+    console.log(`Swagger application is running on: ${url}/api`);
 }
 bootstrap();
