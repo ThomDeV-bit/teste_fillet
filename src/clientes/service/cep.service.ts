f57787ad956ee5d3d55493d74c3708544c7c56ba
Thomaz  josetti <120723852+ThomDeV-bit@users.noreply.github.com>
2025-10-12 11:01:08 -0400

    feat: create cep service and response

---
+++ src/clientes/service/cep.service.ts
@@ -0,0 +1,17 @@
+import  axios from "axios";
+import { plainToInstance } from "class-transformer";
+import { CepResponse } from "../responses/cep.response";
+import { BadRequestException } from "@nestjs/common";
+
+export class CepService {
+    constructor(
+    ){}
+
+    async consultaCep(cep : string): Promise<CepResponse>{
+        const {data} = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+        if(data?.error){
+            throw new BadRequestException('CEP não encontrado!')
+        }
+        return plainToInstance(data , CepResponse)
+    }
+}
