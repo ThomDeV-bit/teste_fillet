ba4069712b7405eb88cfcecb58b176bf0e7e033b
Thomaz  josetti <120723852+ThomDeV-bit@users.noreply.github.com>
2025-10-12 10:58:40 -0400

    feat: create cliente Entity

--- src/clientes/entities/cliente.entity.ts
+++ src/clientes/entities/cliente.entity.ts
@@ -1,1 +1,28 @@
-export class Cliente {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('cliente')
+export class ClienteEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 60 , nullable: false})
+  nomeCompleto: string;
+
+  @Column({ type: 'date',nullable: false })
+  dataNascimento: Date;
+
+  @Column({ length: 14, unique: true })
+  cpf: string;
+
+  @Column({ length: 10 , nullable: false})
+  cep: string;
+
+  @Column({ length: 50 , nullable: false})
+  bairro: string;
+
+  @Column({ length: 50, nullable: false })
+  cidade: string;
+
+  @Column({ length: 2 ,nullable: false})
+  estado: string;
+}
