113ea2fae3f27b1a49a44f6d22b9e60d60b89468
Thomaz  josetti <120723852+ThomDeV-bit@users.noreply.github.com>
2025-10-12 10:58:20 -0400

    fix: add globlal pipes config

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
     const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Cadastro de Clientes')
+    .setDescription('Api de Cadastro de Clientes')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('clientes')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  
+
   await app.listen(process.env.PORT ?? 3000);
+
+  const url = await app.getUrl()
+
+  console.log(`Swagger application is running on: ${url}/api`)
 }
 bootstrap();
